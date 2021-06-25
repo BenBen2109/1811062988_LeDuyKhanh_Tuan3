a20e4226a5fb80410a6354330663c2d61014e41f
LeDuyKhanh_2109 <leduykhanh.ldk21092000@gmail.com>
2021-06-25 11:04:11 +0700

    Add project files.

---
+++ 1811062988_LeDuyKhanh_Tuan3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811062988_LeDuyKhanh_Tuan3.MvcApplication" Language="C#" %>
---
+++ 1811062988_LeDuyKhanh_Tuan3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811062988_LeDuyKhanh_Tuan3.Startup))]
+namespace _1811062988_LeDuyKhanh_Tuan3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
