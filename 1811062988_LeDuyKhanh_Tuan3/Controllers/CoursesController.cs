d7b650b4c55aa0be0b6cc1a5f25e580129be6afd
LeDuyKhanh_2109 <leduykhanh.ldk21092000@gmail.com>
2021-07-13 22:59:31 +0700

    update 2

---
+++ 1811062988_LeDuyKhanh_Tuan3/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1811062988_LeDuyKhanh_Tuan3.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
